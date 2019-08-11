8d1ae7f4d6b3113e7aff59ed3f4fdaf176724807
SashaYerashoff <39062338+SashaYerashoff@users.noreply.github.com>
2019-08-11 20:13:00 +0300

    Basic Views, models & controllers for MVC Login

---
+++ Models/UserAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLoginRegistration.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please enter your username.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Please, enter your password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Please, confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
